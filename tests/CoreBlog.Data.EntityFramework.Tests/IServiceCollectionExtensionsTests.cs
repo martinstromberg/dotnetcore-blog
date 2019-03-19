3fa14fe1c9f754da2a00f8700f52a09f09cbdafc
Martin Strömberg <contact@martinstromberg.se>
2019-03-19 21:18:11 +0100

    Add XUnit tests for IServiceCollectionExtensions

---
+++ tests/CoreBlog.Data.EntityFramework.Tests/IServiceCollectionExtensionsTests.cs
@@ -0,0 +1,66 @@
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CoreBlog.Data.EntityFramework.Tests {
+    using Data.Abstractions.Posts;
+    using Data.Abstractions.Users;
+    using Data.EntityFramework;
+    using Data.EntityFramework.Posts;
+    using Data.EntityFramework.Users;
+
+    public class IServiceCollectionExtensionsTests {
+        ServiceCollection services;
+
+        public IServiceCollectionExtensionsTests() {
+            services = new ServiceCollection();
+        }
+
+        [Fact]
+        public void AddEntityFrameworkBloggingServices_Adds_Blogging_Context() {
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string> {
+                    ["ConnectionStrings:BloggingDatabase"] = "AddEntityFrameworkBloggingServices_Adds_Blogging_Context"
+                })
+                .Build();
+
+            var foo = services
+                .AddEntityFrameworkBloggingServices(configuration)
+                .BuildServiceProvider()
+                .GetService<BloggingContext>()
+                .Should().NotBeNull().And.BeOfType<BloggingContext>();
+        }
+
+        [Fact]
+        public void AddEntityFrameworkBloggingServices_Adds_IBlogPostRepository_Implementation() {
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string> {
+                    ["ConnectionStrings:BloggingDatabase"] = "AddEntityFrameworkBloggingServices_Adds_IBlogPostRepository_Implementation"
+                })
+                .Build();
+
+            var foo = services
+                .AddEntityFrameworkBloggingServices(configuration)
+                .BuildServiceProvider()
+                .GetService<IBlogPostRepository>()
+                .Should().NotBeNull().And.BeOfType<BlogPostRepository>();
+        }
+
+        [Fact]
+        public void AddEntityFrameworkBloggingServices_Adds_IUserRepository_Implementation() {
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new Dictionary<string, string> {
+                    ["ConnectionStrings:BloggingDatabase"] = "AddEntityFrameworkBloggingServices_Adds_IUserRepository_Implementation"
+                })
+                .Build();
+
+            var foo = services
+                .AddEntityFrameworkBloggingServices(configuration)
+                .BuildServiceProvider()
+                .GetService<IUserRepository>()
+                .Should().NotBeNull().And.BeOfType<UserRepository>();
+        }
+    }
+}
