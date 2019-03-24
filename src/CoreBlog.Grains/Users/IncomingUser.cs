88afcf4896ca559bea16c2a7fe9f2568ef32120f
Martin Strömberg <contact@martinstromberg.se>
2019-03-24 18:03:28 +0100

    Add UserModelExtensionMethods tests

---
+++ src/CoreBlog.Grains/Users/IncomingUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using CoreBlog.Data.Abstractions.Users;
+
+namespace CoreBlog.Grains.Users {
+    public class IncomingUser : IUser {
+        public Guid UserId { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+}
--- src/CoreBlog.Grains/Users/UserModelExtensionMethods.cs
+++ src/CoreBlog.Grains/Users/UserModelExtensionMethods.cs
@@ -3,9 +3,17 @@
     using Data.Abstractions.Users;
     using GrainModels.Users;
 
-    public static class IUserExtensionMethods {
+    public static class UserModelExtensionMethods {
         public static User ToGrainModel(this IUser user) {
             return new User {
+                UserId = user.UserId,
+                DisplayName = user.DisplayName,
+                EmailAddress = user.EmailAddress,
+            };
+        }
+
+        public static IUser ToDataModel(this User user) {
+            return new IncomingUser {
                 UserId = user.UserId,
                 DisplayName = user.DisplayName,
                 EmailAddress = user.EmailAddress,
